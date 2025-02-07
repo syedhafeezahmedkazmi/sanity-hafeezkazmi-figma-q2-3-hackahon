fa4cd40386b5069a87d5a9b0e99dbbb76a7be1e5
syedhafeezahmedkazmi <121306021+syedhafeezahmedkazmi@users.noreply.github.com>
2025-02-07 21:47:26 +0500

    page.tsx

--- src/app/myaccount/page.tsx
+++ src/app/myaccount/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Header from '../components/header';
+// import Header from '../components/header';
 import Field from '../components/Feild';
 
 function MyAccount() {
